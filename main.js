64eb6ba1d07814996893d125836900ba9392a90d
Serkan Gür <9862732+ObsessiveCompulsiveAudiophile@users.noreply.github.com>
2025-05-15 14:58:18 +0300

    Add files via upload
    Signed-off-by: Serkan Gür <9862732+ObsessiveCompulsiveAudiophile@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1222,7 +1222,7 @@ async function mainMenu() {
     }}
 async function initializeApp() {
     console.log('--------------------------------');
-    console.log('  A1 Evo Acoustica v5.2 by OCA');
+    console.log('  A1 Evo Acoustica v5.3 by OCA');
     console.log('--------------------------------');
     mainServer = http.createServer((req, res) => {
         const requestUrl = new URL(req.url, `http://${req.headers.host}`);
